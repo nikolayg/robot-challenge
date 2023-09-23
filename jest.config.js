f2341313ce14462936fb06be9f0bcd47b359ed93
Nikolay Grozev <nikolay.grozev@gmail.com>
2023-09-23 20:30:40 +1000

    Config desired coverage and other test config.

--- jest.config.js
+++ jest.config.js
@@ -9,17 +9,14 @@ module.exports = {
   },
   testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx)$",
   coverageReporters: ["json", "lcov", "text"],
-  coveragePathIgnorePatterns: [".*/src/.*\\.d\\.ts", ".*/src/testUtil/.*.ts", ".*/src/index.ts", ".*/src/server.ts", ".*/src/mongo.ts"],
 
-  setupFiles: ["dotenv/config"],
-  // TODO: increase to 80+ each
   coverageThreshold: {
     global: {
-      statements: 1,
-      branches: 1,
-      functions: 1,
-      lines: 1
+      statements: 70,
+      branches: 70,
+      functions: 70,
+      lines: 70
     }
   },
-  testTimeout: 100000
+  testTimeout: 10000
 };
